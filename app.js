7d8896f17bc465cc7199310dfa8fdc3cbe3b3f53
NikoAlyv <enihad801@gmail.com>
2024-03-10 21:15:08 +0400

    Task

---
+++ app.js
@@ -0,0 +1,15 @@
+
+//TODO Task
+
+//! Part1
+
+// function createDelay() {
+//     // create promise
+//   return new Promise((resolve, reject) => {
+//     // 2000 millisecond later
+//     setTimeout(()=>resolve("Delay completed"), 2000);
+//   });
+// }
+
+// console.log(createDelay());
+
