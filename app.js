f2942cb27f7592d7558851c520bc3ea0af67b54e
NikoAlyv <enihad801@gmail.com>
2024-03-10 21:16:26 +0400

    Part2

--- app.js
+++ app.js
@@ -13,3 +13,19 @@
 
 // console.log(createDelay());
 
+//! Part2
+// function createDelay() {
+//   // create promise
+//   const promise = new Promise((resolve, reject) => {
+//     // 1000 millisecond later
+//     setTimeout(() => {
+//       resolve("Delay completed");
+//     }, 1000);
+//   });
+
+//   promise
+//     .then((response) => console.log(response))
+//     .catch((err) => console.log(err));
+// }
+
+// createDelay();
